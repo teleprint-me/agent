b71b39e2c47d1443dba91fe4f7d947585555cb7a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-05-19 22:40:40 -0400

    tools(feat): Add a wrapper function for agent usage

---
+++ agent/tools/weather.py
@@ -0,0 +1,37 @@
+"""
+agent.tools.weather
+"""
+
+from time import sleep
+
+from agent.api.wttr import Weather
+
+
+def get_weather(location: str, unit: str = "metric") -> str:
+    """
+    Get the current weather in a given location.
+    Parameters:
+    location (str): The city and state, e.g. San Francisco, CA
+    unit (str): The unit system, can be either 'metric' or 'uscs'. Default is 'metric'.
+    Returns:
+    str: A string that describes the current weather.
+    """
+    # Set the API response formatting
+    wx = Weather()
+    result = wx.get_custom(location, fmt="%l+%T+%S+%s+%C+%w+%t", unit=unit)
+    return wx.denormalize(result)
+
+
+if __name__ == "__main__":
+    __timeout__ = 1 / (1000 / 3600)  # be kind to beautiful services like wttr!
+    locations = [
+        "",
+        "New York City, New York",
+        "Paris, France",
+        "Tokyo, Japan",
+        "London",
+    ]
+    print(f"Timeout per cycle: {__timeout__}")
+    for location in locations:
+        sleep(__timeout__)
+        print(get_weather(location))
