e7096ea2a4ecf4e47d2d0cd3bb7b810178cc61cb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-11-24 19:16:41 -0500

    app(feat): Enable formatting a gutter with line numbers.

--- agent/examples/pt/app.py
+++ agent/examples/pt/app.py
@@ -4,6 +4,7 @@ https://python-prompt-toolkit.readthedocs.io/en/master/pages/full_screen_apps.ht
 
 from prompt_toolkit import Application
 from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.formatted_text import ANSI
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout.containers import HSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
@@ -38,6 +39,18 @@ def detect_lexer(text: str, path: str = None) -> PygmentsLexer:
         lexer = TextLexer()
 
     return PygmentsLexer(lexer)
+
+
+def line_number_control(buffer: Buffer) -> FormattedTextControl:
+    def get_line_format() -> ANSI:
+        total = buffer.document.line_count
+        width = len(str(total))  # right-pad alignment
+        text = ""
+        for i in range(1, total + 1):
+            text += f"{str(i).rjust(width)}\n"
+        return ANSI(text)
+
+    return FormattedTextControl(get_line_format)
 
 
 layout = Layout(root_container)
