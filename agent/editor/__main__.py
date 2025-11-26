1668de22f849c239751ffb5a26d812c69eb3af20
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-11-25 22:41:33 -0500

    editor(chore): Move core functions to top of module.

--- agent/editor/__main__.py
+++ agent/editor/__main__.py
@@ -9,7 +9,6 @@ from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.document import Document
 from prompt_toolkit.enums import EditingMode
 from prompt_toolkit.filters import Condition
-from prompt_toolkit.formatted_text import ANSI
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.key_binding.key_processor import KeyPressEvent
 from prompt_toolkit.layout.containers import Window
@@ -26,6 +25,58 @@ from agent.config.style import style_dark
 TAB_WIDTH = 4
 
 kb = KeyBindings()
+
+#
+# Lexical Analysis
+#
+
+
+# this is so hacky - i hate it.
+def detect_lexer(text: str = None) -> PygmentsLexer:
+    if text is None:
+        text = ""  # default to TextLexer
+
+    try:
+        # raises ClassNotFound if not a file-like object
+        cls = get_lexer_for_filename(text).__class__
+    except ClassNotFound:
+        # defaults to TextLexer
+        cls = guess_lexer(text).__class__
+
+    # expects a class, not an instance
+    return PygmentsLexer(cls)
+
+
+#
+# on quit
+#
+
+
+@kb.add("c-q")
+def on_quit(event: KeyPressEvent):
+    event.app.exit()
+
+
+#
+# on enter
+#
+
+
+@kb.add("enter")
+def on_enter(event: KeyPressEvent):
+    # current buffer and text
+    buffer = event.current_buffer
+    text = buffer.text
+
+    # detect new lexer
+    new_lexer = detect_lexer(text)
+    event.app.layout.container.content.lexer = new_lexer
+
+    # redraw ui
+    event.app.invalidate()
+    # reset to default behavior
+    buffer.newline()
+
 
 #
 # on tab
@@ -111,53 +162,6 @@ def on_shift_tab(event: KeyPressEvent):
 
     # Dedent current line
     dedent_current_line(buf, doc)
-
-
-#
-# on enter
-#
-
-
-@kb.add("enter")
-def on_enter(event: KeyPressEvent):
-    # current buffer and text
-    buffer = event.current_buffer
-    text = buffer.text
-
-    # detect new lexer
-    new_lexer = detect_lexer(text)
-    event.app.layout.container.content.lexer = new_lexer
-
-    # redraw ui
-    event.app.invalidate()
-    # reset to default behavior
-    buffer.newline()
-
-
-#
-# on quit
-#
-
-
-@kb.add("c-q")
-def on_quit(event: KeyPressEvent):
-    event.app.exit()
-
-
-# this is so hacky - i hate it.
-def detect_lexer(text: str = None) -> PygmentsLexer:
-    if text is None:
-        text = ""  # default to TextLexer
-
-    try:
-        # raises ClassNotFound if not a file-like object
-        cls = get_lexer_for_filename(text).__class__
-    except ClassNotFound:
-        # defaults to TextLexer
-        cls = guess_lexer(text).__class__
-
-    # expects a class, not an instance
-    return PygmentsLexer(cls)
 
 
 if __name__ == "__main__":
