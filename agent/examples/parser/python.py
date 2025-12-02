9fbe35d071a700650b83e2bc5fd448bb1891e219
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-12-01 23:13:43 -0500

    python(feat): Compute start and end lines
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/examples/parser/python.py
+++ agent/examples/parser/python.py
@@ -5,6 +5,7 @@ https://docs.python.org/3/library/ast.html
 import ast
 from argparse import ArgumentParser, Namespace
 from typing import List
+import re
 
 parser = ArgumentParser()
 parser.add_argument("file", type=str, help="The python source file to parse.")
@@ -18,4 +19,24 @@ lines = source.splitlines(keepends=True)
 tree = ast.parse(source)
 print(tree)
 for node in tree.body:
-    print(node)
+    start = node.lineno - 1
+
+    # compute final line
+    first_indent = len(lines[start]) - len(lines[start].lstrip())
+    end = start + 1
+    n = len(lines)
+    while end < n:
+        line = lines[end]
+        # empty/comment lines allowed
+        if not line.strip():
+            end += 1
+            continue
+
+        indent = len(line) - len(line.lstrip())
+        if indent <= first_indent:
+            break
+
+        end += 1
+
+    # start is inclusive, end is exclusive
+    print(f"start: {start + 1}, end: {end}, {node}")
