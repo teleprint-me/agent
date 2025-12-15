0d6740b2a5856e9ac1e70411937bd66b90e3b250
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-12-15 04:29:30 -0500

    mime(refactor): Automate file discovery to simplify sampling
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/parser/mime.py
+++ agent/parser/mime.py
@@ -5,12 +5,11 @@ Get the mime type of a given file object.
 
 import inspect
 from argparse import ArgumentParser
-
+import os
+from pathlib import Path
 import magic
+from typing import Iterator
 
-parser = ArgumentParser()
-parser.add_argument("path", help="Path to plain text source file")
-args = parser.parse_args()
 
 # note that FileMagic has no internal __dict__.
 # use dir instead of vars
@@ -18,14 +17,41 @@ args = parser.parse_args()
 #   - encoding, e.g. utf-8
 #   - mime_type, e.g. text/plain
 #   - name, e.g. Unicode text, UTF-8 text
-file_magic = magic.detect_from_filename(args.path)
+def log_magic(path: Path):
+    file_magic = magic.detect_from_filename(path)
 
-print(f"FileMagic.encoding = '{file_magic.encoding}'")
-print(f"FileMagic.mime_type = '{file_magic.mime_type}'")
-print(f"FileMagic.name = '{file_magic.name}'")
+    print(f"Path: {path}")
+    print(f"FileMagic.encoding = '{file_magic.encoding}'")
+    print(f"FileMagic.mime_type = '{file_magic.mime_type}'")
+    print(f"FileMagic.name = '{file_magic.name}'")
 
-if file_magic.encoding == "binary":
-    print("File is currently unsupported")
-    exit(1)
 
-print("File is a plain text source file")
+def walk_path(root: Path) -> Iterator[Path]:
+    for entry in os.scandir(root):
+        # ignore hidden paths
+        if entry.name.startswith("."):
+            continue
+
+        if entry.is_dir():
+            yield from walk_path(Path(entry.path))
+            continue
+
+        if entry.is_file():
+            yield Path(entry.path)
+
+
+parser = ArgumentParser("python -m agent.parser.mime", description="Guess the mime type of a file")
+parser.add_argument("path", help="Path to a file or directory")
+args = parser.parse_args()
+
+path = Path(args.path)
+if path.is_file():
+    log_magic(path)
+    exit(0)
+
+if not path.is_dir():
+    raise ValueError("Path must be a file or directory")
+
+for entry in walk_path(path):
+    log_magic(entry)
+    print()  # pad with newline
