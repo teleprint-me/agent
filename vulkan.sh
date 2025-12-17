18627d63ef90ba596e205802fdcaa0d47be3120d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-12-17 17:34:48 -0500

    vulkan(refactor): Automate adding core dependencies
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- vulkan.sh
+++ vulkan.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#
 # NOTE: The user is responsible for installing their own drivers if necessary.
 #
 # Fedora provides the drivers out of the box.
@@ -17,15 +18,46 @@
 
 set -euo pipefail # fail fast
 
+#
+# Error Codes (by power of 2):
+#
+# 1 - user ran script with escalated privileges.
+# 2 - user is unable to escalate privileges with sudo.
+# 4 - user is on a unsupported distribution.
+# 8 - failed to install required packages.
+#
+ERROR_ROOT=1
+ERROR_SUDO=2
+ERROR_DIST=4
+ERROR_PKGS=8
+
 # Do not allow users to run the script as root!
 # Ask for permission on an as needed basis.
 # This means user space commands remain in user space.
 # Root commands use sudo to execute as a super user.
-function root() {
-    if 0 -eq $UID; then
-        echo "Do not run this script as root!"
-        exit 1
+function ask_root() {
+    if [ 0 -eq "$(id -u)" ]; then
+        echo "Do not run this script as root!";
+        exit $ERROR_ROOT;
     fi
+}
+
+# Escalate user privileges.
+# Ask once per script run (cached for ~15 min by sudo)
+function ask_sudo() {
+    if ! command -v sudo >/dev/null; then
+        echo "sudo not available";
+        exit $ERROR_SUDO;
+    fi
+
+    # A quick test that forces the password prompt now.
+    sudo true || { echo "Unable to elevate privileges" >&2; return 2; }
+}
+
+# This is useful for discovering the distro family if the above fails for some reason.
+function ask_os_release() {
+    # https://www.freedesktop.org/software/systemd/man/latest/os-release.html
+    echo "$(grep -m1 '^ID=' /etc/os-release | cut -d'=' -f2)"
 }
 
 # discover the distribution family
@@ -37,42 +69,63 @@ function root() {
 # a more accurate way to handle this would be cat /etc/os-release
 # but then we have to filter out the id and match it.
 # checking for the package manager keeps things simple, but is error prone.
-function distro() {
+function ask_package_manager() {
     if command -v apt >/dev/null; then
-        echo "debian"
+        echo "apt"; # debian
     elif command -v dnf >/dev/null; then
-        echo "fedora"
+        echo "dnf"; # fedora
     elif command -v pacman >/dev/null; then
-        echo "arch"
+        echo "pacman"; # arch
     else
-        echo "unknown"
+        echo "$(ask_os_release)"; # unsupported os
     fi
+}
+
+function ask_vulkan_packages() {
+    case "$(ask_package_manager)" in
+        apt)
+            # note: ubuntu uses mesa-vulkan-drivers instead of vulkan-headers?
+            # https://packages.debian.org/search?keywords=vulkan
+            # https://packages.ubuntu.com/search?keywords=vulkan
+            echo "vulkan-headers libvulkan-dev vulkan-tools"
+            ;;
+        dnf)
+            # note: drivers are installed out of the box by default.
+            # we only need the development packages.
+            # https://packages.fedoraproject.org/search?query=vulkan
+            echo "vulkan-headers vulkan-loader vulkan-tools"
+            ;;
+        pacman)
+            # note: drivers must be explicitly installed by user.
+            # https://wiki.archlinux.org/title/Vulkan
+            # https://archlinux.org/packages/?q=vulkan
+            echo "vulkan-headers vulkan-icd-loader vulkan-tools"
+            ;;
+        *)
+            echo "Unsupported os release: $(ask_os_release)";
+            exit $ERROR_DIST;
+            ;;
+    esac
 }
 
 # llama.cpp depends upon the vulkan headers and icd loader.
 # this varies from distro to distro, but are generally the same.
 # user must attend the install and confirm packages manually.
-function vulkan() {
-    family=$(distro)
-    case $family in
-        debian)
-            # https://wiki.archlinux.org/title/Vulkan
-            sudo apt install vulkan-headers libvulkan-dev vulkan-tools
-        fedora)
-            # https://packages.fedoraproject.org/search?query=vulkan
-            sudo dnf install vulkan-headers vulkan-loader vulkan-tools
-        arch)
-            # https://packages.debian.org/search?keywords=vulkan
-            sudo pacman -S vulkan-headers vulkan-icd-loader vulkan-tools
-        *)
-            echo "Unknown distribution family"
-            exit 1
-    esac
+function install_vulkan_packages() {
+    cmd="$(ask_package_manager)"
+    pkg="$(ask_vulkan_packages)"
+    sudo $cmd $pkg
+
+    if [ 0 -neq $? ]; then
+        echo "Failed to install $pkg";
+        exit $ERROR_PKGS;
+    fi
 }
 
 function main() {
-    root
-    vulkan
+    ask_root
+    ask_sudo
+    install_vulkan_packages
 }
 
 main
