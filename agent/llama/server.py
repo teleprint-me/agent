7e9a4b053a7f76b4ecc92b4a07ace457a6be978c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-12-21 01:57:41 -0500

    server(refactor): Simplify and fix broken logic
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/llama/server.py
+++ agent/llama/server.py
@@ -3,8 +3,11 @@
 
 import shutil
 import time
+from pathlib import Path
 from subprocess import Popen
 from typing import Optional
+
+from requests.exceptions import HTTPError
 
 from agent.config import config
 from agent.llama.requests import LlamaCppRequest
@@ -13,8 +16,9 @@ from agent.llama.requests import LlamaCppRequest
 class LlamaCppServer:
     def __init__(self, request: Optional[LlamaCppRequest] = None):
         self.request = request if request else LlamaCppRequest()
-        self.binary = shutil.which("llama-server")
         self.process = None
+        self.logger = config.get_logger("logger", self.__class__.__name__)
+        self.logger.debug("Initialized LlamaCppServer instance.")
 
     def _wait(self, timeout: float = 30.0) -> bool:
         start = time.time()
@@ -28,8 +32,11 @@ class LlamaCppServer:
             time.sleep(0.25)
         return False
 
-    def has_binary(self) -> bool:
-        return True if self.binary else False
+    def path(self) -> Path:
+        return Path(shutil.which("llama-server"))
+
+    def exists(self) -> bool:
+        return self.path().exists()
 
     def build(cmd: list[str]) -> None:
         # Non-blocking, background process
@@ -48,34 +55,70 @@ class LlamaCppServer:
         return False
 
     def poll(self, timeout: float = 30.0) -> None:
-        print("waiting for server")
+        self.logger.info("waiting for llama-server")
         if not self._wait(timeout):
             health = self.request.health()
             if health.get("error"):
-                error_code = model.health["error"]["code"]
-                error_msg = model.health["error"]["message"]
-                print(f"Error ({error_code}): {error_msg}")
-            print("Server failed to become ready.")
+                error_code = health["error"]["code"]
+                error_msg = health["error"]["message"]
+                self.logger.error(f"Error ({error_code}): {error_msg}")
+            self.logger.error("Server failed to become ready.")
             self.kill()
             exit(1)
-        print(f"llama-server (pid): {self.process.pid}")
+        self.logger.info(f"llama-server (pid): {self.process.pid}")
 
 
 # usage example
 # note: Model presets allow advanced users to define custom configurations using an .ini file
 # llama-server --models-preset ./my-models.ini
 if __name__ == "__main__":
+    import sys
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument("--port", type=int, default=8080)
-    parser.add_argument("--n-gpu-layers", type=int, default=-1)
-    parser.add_argument("--models-dir", default=None)
-    parser.add_argument("--models-preset", default=None)
+    parser.add_argument(
+        "--port",
+        default="8080",
+        help="Port the server listens from",
+    )
+    parser.add_argument(
+        "--n-gpu-layers",
+        type=int,
+        default=-1,
+        help="Number of layers stored in VRAM",
+    )
+    parser.add_argument(
+        "--models-dir",
+        default=None,
+        help="Model directory used by the router",
+    )
+    parser.add_argument(
+        "--models-preset",
+        default=None,
+        help="File used to configure the router",
+    )
     args = parser.parse_args()
 
+    llama_server = LlamaCppServer()
+    if not llama_server.exists():
+        print("llama-server is not available in the system PATH variable")
+        exit(1)
+
+    # I'm not sure how to handle dynamic context size at runtime
+    # --ctx-size: size of the prompt context (default: 0, 0 = loaded from model)
     cmd = [
-        llama_server,
+        # /usr/local/bin/llama-server
+        llama_server.path(),
+        # whether to use jinja template engine for chat (default: enabled)
+        "--jinja",
+        # enable prometheus compatible metrics endpoint (default: disabled)
+        "--metrics",
+        # enable changing global properties via POST /props (default: disabled)
+        "--props",
+        # expose slots monitoring endpoint (default: enabled)
+        "--slots",
+        # use single unified KV buffer shared across all sequences (default: enabled if number of slots is auto)
+        "--kv-unified",
         # port to listen (default: 8080)
         "--port",
         str(args.port),
@@ -85,19 +128,14 @@ if __name__ == "__main__":
     ]
 
     # directory containing models for the router server (default: disabled)
-    if args.model_dir:
+    if args.models_dir:
         cmd.extend(["--models-dir", str(args.models_dir)])
 
     # path to INI file containing model presets for the router server (default: disabled)
-    if args.model_preset:
+    if args.models_preset:
         cmd.extend(["--models-preset", str(args.models_preset)])
 
     for token in cmd:
         print(token, end=" ")
         sys.stdout.flush()
     print()
-
-    llama_server = LlamaCppServer()
-    if llama_server.has_binary():
-        print("llama-server is not available in the system PATH variable")
-        exit(1)
