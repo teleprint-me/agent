c58c95bd3a1214e75b453225b1238ed0f3a96f50
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-12-24 00:05:22 -0500

    client(feat): Add initial draft
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/llama/client.py
+++ agent/llama/client.py
@@ -34,5 +34,44 @@ class LlamaCppClient:
 
 
 # usage example
+# model orchestration may be possible by employing a larger model
+# to orientate and direct smaller models. this may require observation of
+# memory which is not easy to do. i'm considering using a custom API hook
+# into ggmls backend to enable getting memory related usage. for now, i just
+# kind of have to guess. the backend observation must be hardware agnostic.
+# one option that remains simple is to require vulkan which would enable probing
+# just about any gpu without having to plugin to complex interface abstractions.
+# these operations must occur synchronously. every operation depends upon on
+# previous operation and we have to wait for the response before proceeding.
 if __name__ == "__main__":
-    raise NotImplementedError()
+    from argparse import ArgumentParser
+    from pathlib import Path
+
+    parser = ArgumentParser()
+    parser.add_argument("model", help="Path to the model file")
+    args = parser.parse_args()
+
+    # just reference the internal config for now
+    request = LlamaCppRequest()  # this is the base object
+
+    # every instance should probably share the core request object
+    # maybe make the request object a singleton? unsure for now.
+    server = LlamaCppServer(request)  # managers llama-server
+    router = LlamaCppRouter(request)  # manages models
+
+    # start the server
+    server.start()  # optionally accepts args (overrides internal config)
+
+    # the challenge here is deciding how to handle model routing
+    # every endpoint will require a model id reference once a model is selected
+    # for now, i just employ models/gpt-oss-20b-mxfp4.gguf
+    model = str(Path(args.model).stem)
+
+    # once the model is selected, we can load it
+    router.load(model)
+
+    # it's good hygiene to clean up (unnecessary, but good habit)
+    router.unload(model)
+
+    # stop the server
+    server.stop()
