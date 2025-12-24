7b846e92ce9d5b1f3b9217c0cdec30a69999c72d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-12-23 23:51:04 -0500

    client(feat): Add a basic skeleton
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/llama/client.py
+++ agent/llama/client.py
@@ -5,14 +5,32 @@ Copyright © 2025 Austin Berrio
 High-level client for performing language model inference.
 """
 
+from typing import Optional
+
 from agent.config import config
 from agent.llama.requests import LlamaCppRequest
 from agent.llama.router import LlamaCppRouter
 from agent.llama.server import LlamaCppServer
 
 
+class LlamaCppTokenizer:
+    def __init__(self, request: Optional[LlamaCppRequest] = None):
+        raise NotImplementedError()
+
+
+class LlamaCppEmbedding:
+    def __init__(self, request: Optional[LlamaCppRequest] = None):
+        raise NotImplementedError()
+
+
+class LlamaCppCompletion:
+    def __init__(self, request: Optional[LlamaCppRequest] = None):
+        raise NotImplementedError()
+
+
 class LlamaCppClient:
-    raise NotImplementedError()
+    def __init__(self, request: Optional[LlamaCppRequest] = None):
+        raise NotImplementedError()
 
 
 # usage example
