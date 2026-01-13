7e63b157365b81850f4c5a6297fd8db1c8f1dddb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2026-01-12 23:41:18 -0500

    classify(feat): Handle UTF-8 bytes
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/text/classify.py
+++ agent/text/classify.py
@@ -11,13 +11,29 @@ Heuristic:
 from pathlib import Path
 
 
+def is_ascii_bytes(data: bytes, threshold: float = 0.30) -> bool:
+    """
+    Return True if at least `threshold` of *data* is printable ASCII or common whitespace.
+    ref: https://stackoverflow.com/a/1446870
+    """
+    # Add control char set
+    control = {b"\n"[0], b"\r"[0], b"\t"[0], b"\b"[0]}
+    # Add printable char set
+    characters = set(range(0x20, 0x7F))
+    # Build a set containing every element from either operand
+    allowed = characters | control
+    # Compute number of raw bytes
+    byte_count = sum(1 for b in data if b not in allowed)
+    # Compute ratio between raw bytes and data
+    byte_ratio = byte_count / len(data)
+    # If more than x % non-text characters, classify as a binary
+    return byte_ratio < threshold
+
+
 def is_text_file(path: Path) -> bool:
     """
     Read the first 512 bytes of *path* and classify it.
     Return True if `data` looks like plain ASCII/UTF-8 text.
-    Source - https://stackoverflow.com/a/1446870
-    Posted by Thomas, modified by community. See post 'Timeline' for change history
-    Retrieved 2026-01-12, License - CC BY-SA 3.0
     """
 
     with open(path, "rb") as file:
@@ -31,19 +47,13 @@ def is_text_file(path: Path) -> bool:
         if 0 in data:
             return False
 
-        # Add control char set
-        control = {b"\n"[0], b"\r"[0], b"\t"[0], b"\b"[0]}
-        # Add printable char set
-        characters = set(range(0x20, 0x7F))
-        # Build a set containing every element from either operand
-        allowed = characters | control
-        # Compute number of raw bytes
-        byte_count = sum(1 for b in data if b not in allowed)
-        # Compute ratio between raw bytes and data
-        byte_ratio = byte_count / len(data)
+        if is_ascii_bytes(data):
+            return True
 
-    # If more than 30% non-text characters, classify as a binary file
-    return False if byte_ratio >= 0.30 else True
+    try:
+        return bool(data.decode())
+    except UnicodeDecodeError:
+        return False
 
 
 if __name__ == "__main__":
