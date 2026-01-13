d781ce27c2a81ae11da52a2c414a70750a20c32e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2026-01-13 03:22:46 -0500

    classify(feat): Enable mining file meta data
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/text/classify.py
+++ agent/text/classify.py
@@ -12,7 +12,31 @@ Reference: https://stackoverflow.com/a/1446870/15147156
 
 from pathlib import Path
 
+# some kind of black magic going on with str == bytes
 from magic import detect_from_filename
+
+# there has to be a better way than this 🫠
+EXT_TO_CLS = {
+    ".txt": "text",
+    ".c": "c",
+    ".h": "c",
+    ".cpp": "cpp",
+    ".hpp": "cpp",
+    ".rs": "rust",
+    ".py": "python",
+    ".sh": "bash",
+    ".md": "markdown",
+    ".htm": "html",
+    ".html": "html",
+    ".css": "css",
+    ".json": "json",
+    ".js": "javascript",
+    ".mjs": "javascript",
+    ".pdf": "postscript",
+    ".png": "image",
+    ".jpg": "image",
+    ".jpeg": "image",
+}
 
 
 def is_ascii(data: bytes, threshold: float = 0.30) -> bool:
@@ -76,29 +100,47 @@ def is_text(data: bytes, threshold: float = 0.30) -> bool:
     return False
 
 
-def classify(path: Path) -> dict[str, any]:
+def magic(path: Path) -> dict[str, any]:
     path = Path(path).resolve()
     magic = detect_from_filename(path)
     data = path.read_bytes()[:512]
 
+    # this should be serializable (se-​ri-​al-​iza-ble 🧐)
     return {
         "text": is_text(data),
         "path": str(path),
         "parent": str(path.parent),
         "stem": path.stem,
         "size": path.stat().st_size,
-        "suffix": getattr(path, "suffix", None),
+        "suffix": path.suffix or None,
+        "class": EXT_TO_CLS.get(path.suffix, None),
         "mime_type": getattr(magic, "mime_type", None),
         "encoding": getattr(magic, "encoding", None),
     }
 
 
+def collect(path: Path):
+    path = Path(path)
+
+    if path.is_file():
+        return [magic(path)]
+
+    collection = []
+    for dirname, _, filenames in os.walk(args.path):
+        for name in filenames:
+            filepath = Path(f"{dirname}/{name}")
+            collection.append(magic(filepath))
+    return collection
+
+
 if __name__ == "__main__":
     import json
+    import os
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
     parser.add_argument("path", help="Input file to classify.")
     args = parser.parse_args()
 
-    print(f"Class: {json.dumps(classify(args.path), indent=2)}")
+    for cls in collect(args.path):
+        print(f"class: {json.dumps(cls, indent=2)}")
