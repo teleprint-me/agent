e1ce4e57f4f05fc404d51159017b0ed7db745835
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2026-01-27 21:50:21 -0500

    pdf(refactor): Simplify CLI args
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/text/pdf.py
+++ agent/text/pdf.py
@@ -23,39 +23,31 @@ if __name__ == "__main__":
 
     from argparse import ArgumentParser, Namespace
 
-    def parse_args() -> Namespace:
-        parser = ArgumentParser(
-            description="Convert a PDF document into text and optionally save or print it."
-        )
-        parser.add_argument(
-            "-i",
-            "--input_path",
-            type=str,
-            required=True,
-            help="The path to the PDF document to be converted.",
-        )
-        parser.add_argument(
-            "-o",
-            "--output_path",
-            type=str,
-            default="",
-            help="The path to save the extracted text. If not provided, the text will be printed to stdout.",
-        )
-        return parser.parse_args()
-
-    args = parse_args()
-    input_path = args.input_path
-    output_path = args.output_path
+    parser = ArgumentParser(
+        description="Convert a PDF document into text and optionally save or print it."
+    )
+    parser.add_argument(
+        "path",
+        help="The path to the PDF document to be converted.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="",
+        help="The path to save the extracted text. If not provided, the text will be printed to stdout.",
+    )
+    args = parser.parse_args()
 
     # Check if the input file exists and is a PDF
-    if not os.path.isfile(input_path) or not input_path.endswith(".pdf"):
+    if not os.path.isfile(args.path) or not args.path.endswith(".pdf"):
         print("Error: The input path must point to a valid PDF file.")
         exit(1)
 
     # Convert the PDF to text
     string = StringIO()
     params = LAParams()
-    with open(args.input_path, "rb") as file:
+    with open(args.path, "rb") as file:
         extract_text_to_fp(file, string, laparams=params, output_type="html", codec=None)
     content = string.getvalue()
     print(content)
