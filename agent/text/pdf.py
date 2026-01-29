a1bf57dd56e47f3b5ff3911a90528a6ae1432a63
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2026-01-28 23:49:42 -0500

    pdf(sketch): Read a raw byte stream
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- agent/text/pdf.py
+++ agent/text/pdf.py
@@ -8,12 +8,36 @@ A Python script for converting PDF files to plaintext.
 """
 
 import os
+from dataclasses import dataclass
 from io import StringIO
 from pathlib import Path
 from typing import List, Union
 
-from pdfminer.high_level import extract_pages, extract_text_to_fp
-from pdfminer.layout import LAParams, LTPage, LTTextContainer
+
+@dataclass(frozen=True)
+class PDFMagic:
+    type: str
+    version: str
+    mime: str
+    path: Path
+
+
+class PDF:
+    def magic(path: str) -> PDFMagic:
+        with open(args.path, "rb") as file:
+            header = file.readline().strip()  # remove newline
+            if not header.startswith(b"%PDF"):
+                raise BufferError("File is not a portable document format.")
+            name, version = header.decode().split("-")
+            type = name[1:].lower()
+            mime = f"application/{type}"
+            return PDFMagic(
+                type=type,
+                version=version,
+                mime=mime,
+                path=Path(path),
+            )
+
 
 # Usage example
 if __name__ == "__main__":
@@ -39,43 +63,6 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    # Check if the input file exists and is a PDF
-    if not os.path.isfile(args.path) or not args.path.endswith(".pdf"):
-        print("Error: The input path must point to a valid PDF file.")
-        exit(1)
-
-    # notes:
-    # - not sure how to extract images yet
-    # - there are no vertical boxes? only horizontal?
-    # - box content is fine. no mods needed.
-    # - vertical bands require manual buffers and mods.
-    #   - a vertical band is a column of text containing a single character
-    # - not sure how to extract tables yet.
-    #   - tables end up flat and without structure.
-    # - can be converted to html, but adds a lot of complexity.
-    #   - tree-sitter could be used, but requires DOM-like traversal.
-    #   - tags have no ids or classes to unique identify elements.
-    #   - styles contain document coordinates, but do not render correctly.
-    # - try to extract text using pdfminer first. then see how it goes from there.
-
-    # Convert the PDF to text
-    string = StringIO()
-    params = LAParams()
-    for page in extract_pages(args.path):  # LTPage
-        print(f"Page: {page.pageid}")
-        # LTLayoutContainer ? not sure which is base yet.
-        # base could be LTContainer, LTComponent, or something else.
-        # docs suggest isinstance to detect element types.
-        for element in page:
-            print(element)
-            if hasattr(element, "get_text"):
-                text = element.get_text()
-                buffer = []  # vertical text
-                for line in text.splitlines():
-                    if len(line) == 1:  # discovered vertical band
-                        buffer.append(line)
-                if buffer:
-                    # appended vertical elements are added in reverse order
-                    # mirror the content to restore lexicographical order
-                    text = "".join(reversed(buffer))
-                print(text)
+    pdf_magic = pdf_magic_factory(args.path)
+    print(pdf_magic)
+    # print(packed[:30])
